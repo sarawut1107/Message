4bb4c3c5478220ab997024dbe691950b607fd0ab
numvarn <phisan.shukkhi@gmail.com>
2021-02-12 12:31:36 +0700

    update add & delete

--- lib/delete.dart
+++ lib/delete.dart
@@ -15,43 +15,54 @@ class DeleteMessage extends StatefulWidget {
 
 class _DeleteMessageState extends State<DeleteMessage> {
   String messageStr = '';
+  DataFileProcess dataFile = DataFileProcess();
+  List<Map> dataList = [];
+  String selectedID;
+  TextEditingController _controller;
 
-  Future<void> _addMessage() async {
-    DataFileProcess dataFile = DataFileProcess();
-    List<Map> dataList = [];
+  Future<void> _deleteMessage() async {
+    dataList.removeWhere((element) => element['id'] == selectedID.toString());
 
-    //Exist data
+    // ทำต่อเอง ให้ดูจากตัวอย่างทีให้ไว้
+
+    // Navigator.push(
+    //     context,
+    //     MaterialPageRoute(
+    //         builder: (context) => MyHomePage(title: 'File Process')));
+  }
+
+  Future<String> _getFile(String id) async {
     String dataStr = await dataFile.readData();
-    var dataJson = jsonDecode(dataStr);
-    for (var item in dataJson) {
-      Map<String, dynamic> dataMap = {
-        'id': item['id'],
-        'msg': item['msg'],
-      };
-      dataList.add(dataMap);
+    if (dataStr != 'fail' && dataStr != '{}' && dataList.length == 0) {
+      var dataJSon;
+      if (dataList.length == 0) {
+        dataJSon = jsonDecode(dataStr);
+        for (var item in dataJSon) {
+          Map<String, dynamic> dataMap = {
+            'id': item['id'],
+            'msg': item['msg'],
+          };
+          dataList.add(dataMap);
+
+          if (item['id'] == id) {
+            setState(() {
+              _controller = TextEditingController(text: item['msg']);
+            });
+          }
+        }
+      }
     }
 
-    //New data
-    Map<String, dynamic> dataMap = {
-      'id': '0',
-      'msg': messageStr,
-    };
-
-    dataList.add(dataMap);
-
-    var dataJson_new = jsonEncode(dataList);
-    dataFile.writeData(dataJson_new.toString());
-
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MyHomePage(title: 'File Process')));
+    return 'success';
   }
 
   @override
   Widget build(BuildContext context) {
+    selectedID = widget.id;
+
     TextField _message = TextField(
       decoration: InputDecoration(hintText: 'Enter message'),
+      controller: _controller,
       onChanged: (value) {
         messageStr = value;
       },
@@ -59,9 +70,9 @@ class _DeleteMessageState extends State<DeleteMessage> {
 
     RaisedButton _addButton = RaisedButton(
       onPressed: () {
-        _addMessage();
+        _deleteMessage();
       },
-      child: Text("Add Message"),
+      child: Text("Delete Message"),
       color: Colors.pink,
     );
 
@@ -69,17 +80,32 @@ class _DeleteMessageState extends State<DeleteMessage> {
       appBar: AppBar(
         title: Text("Delete Message"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            _message,
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[_addButton],
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+        future: _getFile(selectedID),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  _message,
+                  ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: <Widget>[_addButton],
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
